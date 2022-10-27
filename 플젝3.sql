1475090249eab8947863390c2625d1834aca9f7c
nahx2 <108976269+nahx2@users.noreply.github.com>
2022-10-28 00:28:26 +0900

    221024 플젝

---
+++ 플젝3.sql
@@ -0,0 +1,27 @@
+ALTER TABLE tb_subs  MODIFY sub_start VARCHAR2(200);
+
+
+
+ALTER TABLE tb_FAQ  MODIFY faq_write_date VARCHAR2(200);
+
+ALTER TABLE tb_amsg  MODIFY msg_send_date VARCHAR2(200);
+
+ALTER TABLE tb_mdreview  MODIFY md_review_written_date VARCHAR2(200);
+
+ALTER TABLE tb_chatroom  MODIFY chatroom_date VARCHAR2(200);
+
+ALTER TABLE tb_chatlog  MODIFY chat_date VARCHAR2(200);
+
+ALTER TABLE tb_point  MODIFY point_date VARCHAR2(200);
+
+ALTER TABLE tb_msg  MODIFY msg_send_date VARCHAR2(200);
+
+ALTER TABLE tb_reply  MODIFY reply_date VARCHAR2(200);
+
+ALTER TABLE tb_community  MODIFY board_written_date VARCHAR2(200);
+
+
+
+
+EDIT TB_STORE 
+

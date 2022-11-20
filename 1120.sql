188bc3afc8c21c8c87acf49dd4a3a0d7fa46a587
nahx2 <108976269+nahx2@users.noreply.github.com>
2022-11-20 18:18:30 +0900

    Add files via upload

---
+++ 1120.sql
@@ -0,0 +1,24 @@
+SELECT
+             info_no,
+             info_cate,
+             info_date,
+             info_yn
+         FROM
+             info
+             
+             
+             
+             SELECT
+             *
+         FROM
+             info
+             
+             edit INFO 
+             
+             ALTER TABLE info MODIFY info_cate VARCHAR2(4000);
+             
+             
+             ALTER TABLE info MODIFY info_title VARCHAR(4000) NOT NULL;
+             
+             
+            SELECT INFO_NO, INFO_TITLE, INFO_CONTENT, INFO_CATE, INFO_DATE, INFO_YN FROM INFO
